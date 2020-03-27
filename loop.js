60d8dbba558f0249856aa09be168e28473c272da
samuel-osaka <62089587+samuel-osaka@users.noreply.github.com>
2020-03-27 13:48:54 +0100

    JavaScript task

---
+++ loop.js
@@ -0,0 +1,13 @@
+//store name in a varable
+var name = 'Anyaegbunam Samuel';
+console.log(name);
+//store courses in an array
+var courses = ["Html","CSS", "JavaScript","Python"]
+console.log(courses);
+//find and display all even numbers from 1-200 (inclusive)
+var count = 1;
+while(count <= 200){
+	if (count % 2 === 0){console.log(count);
+	}
+	count += 1;
+}
